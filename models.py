e389e26818c6146f93711b462d8e7cecb2f215d3
yoel <daviddonyo@Yoel.local>
2026-07-14 11:14:31 +0300

    Remove ip column from Item model

--- models.py
+++ models.py
@@ -8,4 +8,3 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
-    ip = Column(String, nullable=True)
